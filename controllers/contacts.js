f4f8b65cdd68f0a64d4f875d310624d5216a3aa9
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2023-03-20 11:46:23 +0200

    auth

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,8 @@ const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const result = await Contact.find(owner);
   res.json(result);
 };
 
@@ -18,7 +19,8 @@ const getById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create(...req.body, owner);
   res.status(201).json(result);
 };
 
